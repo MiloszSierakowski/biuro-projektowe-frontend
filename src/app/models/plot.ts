bf2fa7b7ef9a49453747c3f865be2a5b9fd58c0a
Miłosz Sierakowski <roland_m1@wp.pl>
2025-08-23 18:13:24 +0200

    zmiana id na numer w plot.ts

--- src/app/models/plot.ts
+++ src/app/models/plot.ts
@@ -1,5 +1,5 @@
 export interface Plot {
-  id: string | number;   // unikalne ID działki
+  id: number;   // unikalne ID działki
   name: string;          // nazwa działki (np. "Działka 12/7 – Gdynia")
   client: string;        // klient przypisany do działki
 }
